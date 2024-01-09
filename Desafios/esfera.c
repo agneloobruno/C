17b40294066cb306d27bbfcae7634fae5e94a85a
Bruno Agnelo <97995556+agneloobruno@users.noreply.github.com>
2024-01-09 10:53:30 -0400

    Esfera
    Consegui fazer o exercício solicitado pela plataforma beecrowed

--- Desafios/esfera.c
+++ Desafios/esfera.c
@@ -1,7 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
 //Faça um programa que calcule e mostre o volume de uma esfera sendo fornecido o valor de seu raio (R). A fórmula para calcular o volume é: (4/3) * pi * R3. Considere (atribua) para pi o valor 3.14159.
 
-#include <stdio.h>
 
 int main() {
-    
+
+    float raio = 0.0;
+    double volume = 0.0;
+
+    printf("Digite o valor do raio:");
+    scanf("%f", &raio);
+
+    volume = (4/3.0) * 3.14159 * pow(raio, 3);
+
+    printf("VOLUME = %.3f\n", volume);
+
 }
