e4199102a7776027ea836a1616e7798265df9e3b
Bruno Agnelo <97995556+agneloobruno@users.noreply.github.com>
2024-01-11 14:30:11 -0400

    em cosntrução

---
+++ Desafios/ParOuImpar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+
+    int valor1 = 0, valor2 = 0, valor3 = 0, valor4 = 0, valor5 = 0;
+    
+    scanf("%d %d %d %d %d", &valor1,&valor2 ,&valor3 ,&valor4, &valor5);
+
+    
+}
