b2c21a234096d6acf4ef9fb0c4423f4204f42809
Bruno Agnelo <97995556+agneloobruno@users.noreply.github.com>
2024-01-11 11:43:27 -0400

    Gasto de combustivel

--- Desafios/GastoDeCombustivel.c
+++ Desafios/GastoDeCombustivel.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    
+
+    int tempo = 0, velocidade_media = 0;
+    float litros = 0, distancia = 0;
+
+    scanf("%d %d", &tempo, &velocidade_media);
+
+    distancia = tempo * velocidade_media;
+
+    litros = distancia / 12;
+
+    printf("%.3f\n", litros);
 }
