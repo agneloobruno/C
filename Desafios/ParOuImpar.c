b40634bdd16d3534c79330a041a36f275da58611
Bruno Agnelo <97995556+agneloobruno@users.noreply.github.com>
2024-01-12 09:44:40 -0400

    While
    após verificar que tantas variaveis eram inviaveis resolvi testar o comando while para solucção do problema e deu certo

--- Desafios/ParOuImpar.c
+++ Desafios/ParOuImpar.c
@@ -2,9 +2,30 @@
 
 int main() {
 
-    int valor1 = 0, valor2 = 0, valor3 = 0, valor4 = 0, valor5 = 0;
-    
-    scanf("%d %d %d %d %d", &valor1,&valor2 ,&valor3 ,&valor4, &valor5);
+    int valor = 0, cont = 5, pares = 0, impares = 0, positivo = 0, negativo = 0;
 
-    
+    while (cont > 0) {
+
+        scanf("%d", &valor);
+
+        if (valor % 2 == 0) {
+            pares = pares + 1;
+        } else  if (valor % 2 != 0){
+            impares = impares + 1;
+        }
+
+        if (valor > 0) {
+            positivo = positivo + 1;
+        } else if (valor < 0) {
+            negativo = negativo + 1;
+        }
+
+        cont = cont - 1;
+    }
+
+    printf("%d valor(es) par(es)\n", pares);
+    printf("%d valor(es) impar(es)\n", impares);
+    printf("%d valor(es) positivo(s)\n", positivo);
+    printf("%d valor(es) negativo(s)\n", negativo);
+
 }
