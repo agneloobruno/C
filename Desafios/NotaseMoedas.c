5224e6e18d8e67b305cb1685ee20d5ccb2653c21
Bruno Agnelo <97995556+agneloobruno@users.noreply.github.com>
2024-01-16 10:48:01 -0400

    celulas e moedas
    O exercicio que eu achei mais complicado até o momento

--- Desafios/NotaseMoedas.c
+++ Desafios/NotaseMoedas.c
@@ -1,31 +1,63 @@
 #include <stdio.h>
 
 int main() {
-    float valor = 0, moedas = 0, notas = 0;
+    double valor = 0;
+    int notas = 0, moedas = 0;
+    
 
-    scanf("%f", &valor);
+    scanf("%lf", &valor);
 
-    notas = valor / 100;
+    printf("NOTAS:\n");
+
+    notas = (int)valor / 100;
     printf("%d nota(s) de R$ 100.00\n", notas);
-    notas = valor % 100;
+    valor -= notas * 100;
 
-    notas = valor / 50;
-    printf("%d nota(s) de R$ 50.00\n");
-    notas = valor % 50;
+    notas = (int)valor / 50;
+    printf("%d nota(s) de R$ 50.00\n", notas);
+    valor -= notas * 50;
 
-    notas = valor / 20;
-    printf("%d nota(s) de R$ 20.00\n");
-    notas = valor % 20;
+    notas = (int)valor / 20;
+    printf("%d nota(s) de R$ 20.00\n", notas);
+    valor -= notas * 20;
 
-    notas = valor / 10;
-    printf("%d nota(s) de R$ 10.00\n");
-    notas = valor % 10;
+    notas = (int)valor / 10;
+    printf("%d nota(s) de R$ 10.00\n", notas);
+    valor -= notas * 10;
 
-    notas = valor / 5;
-    printf("%d nota(s) de R$ 5.00\n");
-    notas = valor % 5;
+    notas = (int)valor / 5;
+    printf("%d nota(s) de R$ 5.00\n", notas);
+    valor -= notas * 5;
      
-    notas = valor / 2;
-    printf("%d nota(s) de R$ 2.00\n");
-    notas = valor % 2;
+    notas = (int)valor / 2;
+    printf("%d nota(s) de R$ 2.00\n", notas);
+    valor -= notas * 2;
+
+    printf("MOEDAS:\n");
+
+    moedas = valor / 1;
+    printf("%d moeda(s) de R$ 1.00\n", moedas);
+    valor -= moedas * 1;
+
+    moedas = valor / 0.5;
+    printf("%d moeda(s) de R$ 0.50\n", moedas);
+    valor -= moedas * 0.5;
+    
+    moedas = valor / 0.25;
+    printf("%d moeda(s) de R$ 0.25\n", moedas);
+    valor -= moedas * 0.25;
+
+    moedas = valor / 0.10;
+    printf("%d moeda(s) de R$ 0.10\n", moedas);
+    valor -= moedas * 0.10;
+
+    moedas = valor / 0.05;
+    printf("%d moeda(s) de R$ 0.05\n", moedas);
+    valor -= moedas * 0.05;
+
+    moedas = valor / 0.01;
+    printf("%d moeda(s) de R$ 0.01\n", moedas);
+    valor-= moedas * 0.01;
+
+    return 0;
 }
