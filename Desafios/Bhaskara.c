ea34f8567067d453623c361e77f69c70c42d39b0
Bruno Agnelo <97995556+agneloobruno@users.noreply.github.com>
2024-01-16 12:50:02 -0400

    Bhaskara

--- Desafios/Bhaskara.c
+++ Desafios/Bhaskara.c
@@ -1,1 +1,22 @@
 #include <stdio.h>
+#include <math.h>
+
+int main () {
+    double A = 0, B = 0, C = 0, R1 = 0, R2 = 0;
+    int delta = 0;
+
+    delta = B * 2 - 4 * A * C;
+
+    if (delta < 0)
+    {
+        printf("Impossivel calcular");
+    } else {
+        R1 = (-B + sqrt(delta)) / 2 * A;
+
+        R2 = (-B - sqrt(delta)) / 2 * A;
+
+        printf("R1 = %.5lf\n", R1);
+        printf("R2 = %.5lf\n", R2);
+    }
+    
+}
