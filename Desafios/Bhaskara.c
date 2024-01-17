dfcbad6dc60ee250daf6fc5ba587f1072897fb62
Bruno Agnelo <97995556+agneloobruno@users.noreply.github.com>
2024-01-17 14:07:33 -0400

    final
    final

--- Desafios/Bhaskara.c
+++ Desafios/Bhaskara.c
@@ -2,26 +2,14 @@
 #include <math.h>
 
 int main () {
-    double A = 0, B = 0, C = 0, R1 = 0, R2 = 0, teste = 0;
+    double A = 0, B = 0, C = 0, R1 = 0, R2 = 0;
     double delta = 0;
 
     scanf("%lf %lf %lf", &A, &B, &C);
 
-    printf("%lf\n", C);
-
-    teste = 4 * A * C;
-    printf("%lf\n", teste);
-
-    teste = 144 - 4 * A * C;
-
-    printf("teste %lf\n", teste);
-
     delta = pow(B, 2) - 4 * A * C;
 
-    printf("%lf\n", delta);
-    printf("%lf\n", sqrt(delta));
-
-    if (delta < 0)
+    if (delta < 0 || A == 0)
     {
         printf("Impossivel calcular\n");
     } else {
