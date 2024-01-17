08313f07fa0cab3f154587ac1183a1a90448fdfa
Bruno Agnelo <97995556+agneloobruno@users.noreply.github.com>
2024-01-17 13:18:05 -0400

    encontrado erro
    Encontrei o erro do exercicio, um erro besta onde eu estava fazendo a multiplicação de B por dois ao invés de elevar o mesmo ao quadrado, fiz alguns testes antes de descobrir o erro , fui eliminando os possiveis causadores do problema.

--- Desafios/Bhaskara.c
+++ Desafios/Bhaskara.c
@@ -2,18 +2,32 @@
 #include <math.h>
 
 int main () {
-    double A = 0, B = 0, C = 0, R1 = 0, R2 = 0;
-    int delta = 0;
+    double A = 0, B = 0, C = 0, R1 = 0, R2 = 0, teste = 0;
+    double delta = 0;
 
-    delta = B * 2 - 4 * A * C;
+    scanf("%lf %lf %lf", &A, &B, &C);
+
+    printf("%lf\n", C);
+
+    teste = 4 * A * C;
+    printf("%lf\n", teste);
+
+    teste = 144 - 4 * A * C;
+
+    printf("teste %lf\n", teste);
+
+    delta = pow(B, 2) - 4 * A * C;
+
+    printf("%lf\n", delta);
+    printf("%lf\n", sqrt(delta));
 
     if (delta < 0)
     {
-        printf("Impossivel calcular");
+        printf("Impossivel calcular\n");
     } else {
-        R1 = (-B + sqrt(delta)) / 2 * A;
+        R1 = (-B + sqrt(delta)) / (2 * A);
 
-        R2 = (-B - sqrt(delta)) / 2 * A;
+        R2 = (-B - sqrt(delta)) / (2 * A);
 
         printf("R1 = %.5lf\n", R1);
         printf("R2 = %.5lf\n", R2);
