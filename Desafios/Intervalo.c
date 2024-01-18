67b9e165df96d5ee765ab64b58a6323cac87dd9a
Bruno Agnelo <97995556+agneloobruno@users.noreply.github.com>
2024-01-18 10:08:37 -0400

    alteração na primeira condição lógica

--- Desafios/Intervalo.c
+++ Desafios/Intervalo.c
@@ -5,16 +5,16 @@ int main() {
 
     scanf("%f", &valor);
 
-    if (valor > 0 || valor <= 25) {
-        printf("Intervalo [0, 25]\n");
+    if (valor >= 0 || valor < 25) {
+        printf("Intervalo [0,25]\n");
     } else if (valor > 25.00001 || valor <= 50) {
-        printf("Intervalo (25, 50]\n");
+        printf("Intervalo (25,50]\n");
     } else if (valor > 50.00001 || valor <= 75)
     {
-        printf("Intervalo (50, 75]\n");
+        printf("Intervalo (50,75]\n");
     } else if (valor > 75.00001 || valor <= 100)
     {
-        printf("Intervalor (75, 100]\n");
+        printf("Intervalor (75,100]\n");
     } else if (valor < 0 || valor > 100)
     {
         printf("Fora de intervalo");
