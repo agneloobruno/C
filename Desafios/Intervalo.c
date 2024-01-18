b23c4d7c93ab024007fd852cade9e113cbdae145
Bruno Agnelo <97995556+agneloobruno@users.noreply.github.com>
2024-01-18 10:11:32 -0400

    OU para E
    testando a alteração das condições lógicas de OU para E

--- Desafios/Intervalo.c
+++ Desafios/Intervalo.c
@@ -5,18 +5,17 @@ int main() {
 
     scanf("%f", &valor);
 
-    if (valor >= 0 || valor < 25) {
+    if (valor >= 0 && valor <= 25) {
         printf("Intervalo [0,25]\n");
-    } else if (valor > 25.00001 || valor <= 50) {
+    } else if (valor > 25 && valor <= 50) {
         printf("Intervalo (25,50]\n");
-    } else if (valor > 50.00001 || valor <= 75)
+    } else if (valor > 50 && valor <= 75)
     {
         printf("Intervalo (50,75]\n");
-    } else if (valor > 75.00001 || valor <= 100)
+    } else if (valor > 75 && valor <= 100)
     {
         printf("Intervalor (75,100]\n");
-    } else if (valor < 0 || valor > 100)
-    {
+    } else {
         printf("Fora de intervalo");
     }
 }
