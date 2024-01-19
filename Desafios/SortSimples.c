438e27ef9dee1d2741d4b9a8b479cf89d750210e
Bruno Agnelo <97995556+agneloobruno@users.noreply.github.com>
2024-01-19 13:30:44 -0400

    Update SortSimples.c

--- Desafios/SortSimples.c
+++ Desafios/SortSimples.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    int valor1 = 0, valor2 = 0, valor3 = 0;
+    int valor1 = 0, valor2 = 0, valor3 = 0, maior = 0, inter = 0, menor = 0;
 
     scanf("%d %d %d", &valor1, &valor2, &valor3);
 
+    if (valor1 > valor2 && valor1 > valor3 && valor2 > valor3){
+        maior = valor1;
+        inter = valor2;
+        menor = valor3;
+    } else if (valor2 > valor1 && valor2 > valor3 && valor1 > valor3) { 
+        maior = valor2;
+        inter = valor1;
+        menor = valor3;
+    } else if (valor3 > valor1 && valor3 > valor2 && valor2 > valor1) {
+        maior = valor3;
+        inter = valor2;
+        menor = valor1;
+    }
     
+    printf("%d\n %d\n %d\n", maior, inter, menor);
+    printf("%d\n %d\n %d\n", valor1, valor2, valor3);
 }
