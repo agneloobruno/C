7750384df0ec1f6a03b11aa913fe8152fcc61472
Bruno Agnelo <97995556+agneloobruno@users.noreply.github.com>
2024-01-22 13:48:51 -0400

    teste lógico final

--- Desafios/SortSimples.c
+++ Desafios/SortSimples.c
@@ -5,33 +5,23 @@ int main() {
 
     scanf("%d %d %d", &valor1, &valor2, &valor3);
 
-    if (valor1 > valor2 && valor1 > valor3 && valor2 > valor3){
+    if (valor1 >= valor2 && valor1 >= valor3) {
         maior = valor1;
-        inter = valor2;
-        menor = valor3;
-    } else if (valor2 > valor1 && valor2 > valor3 && valor1 > valor3) { 
+        inter = (valor2 >= valor3) ? valor2 : valor3;
+        menor = (valor2 <= valor3) ? valor2 : valor3;
+    } else if (valor2 >= valor3) {
         maior = valor2;
-        inter = valor1;
-        menor = valor3;
-    } else if (valor3 > valor1 && valor3 > valor2 && valor2 > valor1) {
+        inter = (valor1 >= valor3) ? valor1 : valor3;
+        menor = (valor1 <= valor3) ? valor1 : valor3;
+    } else {
         maior = valor3;
-        inter = valor2;
-        menor = valor1;
-    } else if (valor1 > valor2 && valor1 > valor2 && valor3 > valor2) {
-        maior = valor1;
-        inter = valor3;
-        menor = valor2;
-    } else if (valor2 > valor1 && valor2 > valor3 && valor3 > valor1) {
-        maior = valor2;
-        inter = valor3;
-        menor = valor1;
-    } else if (valor3 > valor1 && valor3 > valor2 && valor1 > valor2) {
-        maior = valor3;
-        inter = valor1;
-        menor = valor2;
+        inter = (valor1 >= valor2) ? valor1 : valor2;
+        menor = (valor1 <= valor2) ? valor1 : valor2;
     }
-    
-    printf("%d\n %d\n %d\n", menor, inter, maior);
+
+    printf("%d\n%d\n%d\n", menor, inter, maior);
     printf("\n");
-    printf("%d\n %d\n %d\n", valor1, valor2, valor3);
+    printf("%d\n%d\n%d\n", valor1, valor2, valor3);
+
+    return 0;
 }
