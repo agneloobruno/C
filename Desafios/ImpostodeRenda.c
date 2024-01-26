fe23a3c3122531f795d2868a89cca41688a3ba46
Bruno Agnelo <97995556+agneloobruno@users.noreply.github.com>
2024-01-26 13:48:23 -0400

    teste 2

--- Desafios/ImpostodeRenda.c
+++ Desafios/ImpostodeRenda.c
@@ -2,23 +2,16 @@
 
 int main() {
 
-    float salario = 0, parte_tributavel_1 = 0, parte_tributavel_2 = 0, faixa_4 = 0;
+    float salario = 0, parte_tributavel_1 = 0, parte_tributavel_2 = 0;
 
     scanf("%f", &salario);
 
     if (salario > 0 && salario <= 2000) {
         printf("Isento\n");
-    } else {
+    } else if (salario > 2000.01 && salario <= 3000) {
         salario += - 2000;
-        if (salario > 0 && salario <= 1000) {
-            parte_tributavel_1 = salario * 0.08;
-        } else {
-            salario += - 1000;
-            if (salario > 0 && salario <= 1500) {
-                parte_tributavel_1 = salario * 0.08;
-                salario += - 1000;
-                parte_tributavel_2 = salario * 0.18;
-            } else 
-        }
+        parte_tributavel_1 = salario * 0.08;
+        
     }
+    printf("%.2f %.2f", parte_tributavel_1, parte_tributavel_2);
 }
