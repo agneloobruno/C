1b4c8dca10b357471317d82c576d27f4d87ada38
Bruno Agnelo <97995556+agneloobruno@users.noreply.github.com>
2024-02-01 11:22:56 -0400

    comentário sobre buffer

--- Desafios/Cadastro.c
+++ Desafios/Cadastro.c
@@ -1,3 +1,6 @@
+//Faça um programa que pergunte o nome, a idade, o salario e sexo (M ou F) de uma pessoa e exiba essa informação para ele. Assuma que o nome não será maior que 100 caracteres
+
+
 #include <stdio.h>
 
 int main() {
@@ -13,6 +16,8 @@ int main() {
     scanf("%d", &idade);
 
     while ((getchar()) != '\n');
+
+    /*Nesse ponto do programa tive dificuldades nas primeiras vezes que o rodei porque quando o programa printava no console 'Sexo:' ele não esperava eu digitar o caractere e em seguida já iniciava o outro comando. Após pesquisar a causa do problema, identifiquei que seria necessário limpar o buffer por quê ao lermos diferentes tipos de dados existe a possibilidade de ficar dados residuais que atrapalham a execução do código.*/
 
     printf("Sexo:");
     scanf("%c", &sexo);
