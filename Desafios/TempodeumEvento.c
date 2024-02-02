e2e0f6e82e2b0ac11b947287caf5eb7810b1e37a
Bruno Agnelo <97995556+agneloobruno@users.noreply.github.com>
2024-02-02 09:33:31 -0400

    TempodeumEvento.c
    Tem como objetivo analisar os dados de entrada e dizer quanto tempo durou um evento. Estou tendo certas dificuldades tendo em vista que não consegui fazer o código ler corretamente os dados de entrada.

--- Desafios/TempodeumEvento.c
+++ Desafios/TempodeumEvento.c
@@ -2,10 +2,25 @@
 
 int main() {
 
-    int hora = 0, minuto = 0, segundo = 0, hora_final = 0, minuto_final = 0, segundo_final = 0;
+    int data = 0, hora = 0, minuto = 0, segundo = 0, data_final = 0, hora_final = 0, minuto_final = 0, segundo_final = 0;
+    char dia[3] = "";
+    char dia_final[3] = "";
     
+    //Leitura do dia e da data de inicio do evento
+    scanf("%[^\n]s", &dia);
+    scanf("%d", &data);
 
-    scanf("%d", &dia);
+    scanf("%d : %d : %d", &hora, &minuto, &segundo);
+
+    //Leitura dos dados de fim do evento
+    scanf("%[^\n]s", &dia_final);
+    scanf("%d", &data_final);
+
+    scanf("%d : %d : %d", &hora_final, &minuto_final, &segundo_final);
+
+
+
+
 
 
 }
