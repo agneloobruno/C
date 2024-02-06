260d0a9ee5156dd0df768e21c73b6e0dd93e6697
Bruno Agnelo <97995556+agneloobruno@users.noreply.github.com>
2024-02-06 13:33:05 -0400

    a

--- SomadeImparesConsecutivos.c
+++ SomadeImparesConsecutivos.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 
 int main() {
-    int X = 0, Y = 0;
-
-    scanf("%d %d", &X, &Y);
-
-    if(X % 2 == 0) {
-        X++;
+    int N;
+    scanf("%d", &N); // Lê a quantidade de casos de teste
+    
+    for (int i = 0; i < N; i++) {
+        int X, Y;
+        scanf("%d %d", &X, &Y); // Lê os valores X e Y
+        
+        int soma = 0;
+        int contador = 0;
+        
+        while (contador < Y) {
+            if (X % 2 != 0) { // Se X for ímpar, adiciona X à soma
+                soma += X;
+                contador++;
+            }
+            X++; // Passa para o próximo número
+        }
+        
+        printf("%d\n", soma); // Imprime a soma dos números ímpares consecutivos
     }
-
-    while(Y != 0){
-        X = X + 2;
-    }
-
-    printf("%d", X);
     
     return 0;
 }
