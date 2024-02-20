d80793f0b1603db75c8952b14c3e24cb32f7b951
Bruno Agnelo <97995556+agneloobruno@users.noreply.github.com>
2024-02-20 14:11:44 -0400

    s

--- Desafios/Intervalo_2.c
+++ Desafios/Intervalo_2.c
@@ -1,22 +1,26 @@
 #include<stdio.h>
 
 int main() {
-    int n, i, cont_out, cont_in = 0, x[n];
+    int n, cont_out = 0, cont_in = 0;
 
     scanf("%d", &n);
+
+    int x[n];
 
     /*
        laço de repetição para armazenar os casos de testes
        e realizar a verificação da condição lógica.
     */
-    while (i < n) {
-        scanf("%d", &x[i]);
-        i += 1;
 
-        if (x[i] < 10 && x[i] > 20) {
-            cont_out += 1; 
-        } else if (x[i] > 10 && x[i] < 20) {
-            cont_in += 1;
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (x[i] >= 10 && x[i] <= 20) {
+            cont_in += 1; 
+        } else {
+            cont_out += 1;
         }
     }
 
