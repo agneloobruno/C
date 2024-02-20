8d31403c6d4cbd3ec3a95cdd9ec15caa2eb1b30f
Bruno Agnelo <97995556+agneloobruno@users.noreply.github.com>
2024-02-20 14:44:29 -0400

    a

---
+++ Desafios/QuadradodePares.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+double quadrado(double x) {
+    return x * x;
+}
+
+int main() {
+    int n = 0;
+
+    scanf("%d", &n);
+
+    for(int i = 1; i < n + 1; i++) {
+        if(i % 2 == 0){
+            printf("%d^2 = %.0lf\n", i, quadrado((double)i));
+        }
+    }
+
+    return 0;
+}
